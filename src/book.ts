8102f1a2662941c907c2bd90a38a2517dc42ca8a
Andrea Reni <mail@nbl7.net>
2021-03-10 09:02:24 +0100

    manipulated repo.

--- src/book.ts
+++ src/book.ts
@@ -7,5 +7,49 @@
 import uranio from 'uranio';
 
 export const atom_book:uranio.types.Book = {
-
+	media: {
+		api: {
+			url: 'media'
+		},
+		properties:{
+			src:{
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'src',
+				optional: true
+			}
+		}
+	},
+	products: {
+		api: {
+			url: 'products'
+		},
+		properties: {
+			title: {
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'Title'
+			}
+		}
+	},
+	products2: {
+		api: {
+			url: 'products'
+		},
+		properties: {
+			title: {
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'Title'
+			}
+		}
+	},
+	products9: {
+		api: {
+			url: 'products'
+		},
+		properties: {
+			title: {
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'Title'
+			}
+		}
+	}
 };
