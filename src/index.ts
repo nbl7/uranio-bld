0b895a2a37ef692db358e586157e28ca3542201e
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-10 09:02:24 +0100

    manipulated repo.

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@
  * @packageDocumentation
  */
 
-import {urn_log} from 'urn-lib';
-
-urn_log.defaults.log_level = urn_log.LogLevel.FUNCTION_DEBUG;
+console.log('AAAAAs');
 
 import uranio from 'uranio';
+
+uranio.lib.log.defaults.log_level = uranio.lib.log.LogLevel.FUNCTION_DEBUG;
 
 const express_service = uranio.service.express.create();
 
 express_service.listen(3000, () => {
-	urn_log.debug(`Listening on port 3000...`);
+	uranio.lib.log.debug(`Listening on port 3000...`);
 });
+
+const media_bll = uranio.core.bll.basic.create('media');
+media_bll.find({}).then((d)=>console.log(d));
+
+
