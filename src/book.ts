50b484699912bb165d3e0dc8d64876500e1b0821
Andrea Reni <mail@nbl7.net>
2021-03-11 08:37:26 +0100

    updated.

--- src/book.ts
+++ src/book.ts
@@ -7,49 +7,5 @@
 import uranio from 'uranio';
 
 export const atom_book:uranio.types.Book = {
-	media: {
-		api: {
-			url: 'media'
-		},
-		properties:{
-			src:{
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'src',
-				optional: true
-			}
-		}
-	},
-	products: {
-		api: {
-			url: 'products'
-		},
-		properties: {
-			title: {
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'Title'
-			}
-		}
-	},
-	products2ee: {
-		api: {
-			url: 'products'
-		},
-		properties: {
-			title: {
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'Title'
-			}
-		}
-	},
-	products9: {
-		api: {
-			url: 'products'
-		},
-		properties: {
-			title: {
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'Title'
-			}
-		}
-	}
+
 };
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,3 @@ const express_service = uranio.service.express.create();
 express_service.listen(3000, () => {
 	uranio.lib.log.debug(`Listening on port 3000...`);
 });
-
-// const media_bll = uranio.core.bll.basic.create('media');
-// media_bll.find({}).then((d)=>console.log(d));
-
-
