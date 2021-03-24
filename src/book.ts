7ddcf200b4b7897db8a7275d24572988cb078dfa
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-24 11:20:46 +0100

    new atom_books. removed .urnrc.json

--- src/book.ts
+++ src/book.ts
@@ -7,5 +7,26 @@
 import uranio from 'uranio';
 
 export const atom_book:uranio.types.Book = {
-
+	product:{
+		api: {
+			url: 'products'
+		},
+		properties:{
+			title:{
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'Title'
+			}
+		}
+	},
+	media:{
+		api: {
+			url: 'medias',
+		},
+		properties:{
+			title:{
+				type: uranio.types.BookPropertyType.TEXT,
+				label: 'Title'
+			}
+		}
+	}
 };
