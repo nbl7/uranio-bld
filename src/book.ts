c247685a67633ad0505379c94e4ab9bc89d191b9
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-27 11:26:31 +0100

    removed book atoms.

--- src/book.ts
+++ src/book.ts
@@ -7,26 +7,5 @@
 import uranio from 'uranio';
 
 export const atom_book:uranio.types.Book = {
-	prodee:{
-		properties: {
-			title: {
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'Title'
-			}
-		},
-		api:{
-			url: 'prods'
-		}
-	},
-	prod76ss:{
-		properties: {
-			t:{
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'd'
-			}
-		},
-		api:{
-			url: 'prod23'
-		}
-	}
+
 };
