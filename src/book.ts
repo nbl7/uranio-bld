b7329497f24701d2e2e2769c69faebe82b1440ea
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-06-28 11:56:24 +0200

    update.

--- src/book.ts
+++ src/book.ts
@@ -6,21 +6,6 @@
 
 import uranio from 'uranio';
 
-import {MYBLL} from './server/custom';
-
 export const atom_book:uranio.types.Book = {
-	mykart:{
-		properties:{
-			title: {
-				type: uranio.types.BookPropertyType.TEXT,
-				label: 'Title'
-			}
-		},
-		bll: () => {
-			return new MYBLL();
-		},
-		api: {
-			
-		}
-	}
+
 };
