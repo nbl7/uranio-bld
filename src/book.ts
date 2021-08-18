aceb177453d1f413276b9afa8c9d08ca990684bc
Andrea Reni <mail@nbl7.net>
2021-08-18 11:42:20 +0200

    added atom and route.

--- src/book.ts
+++ src/book.ts
@@ -30,7 +30,7 @@ export const atom_book:uranio.types.Book = {
 					call: async (api_request:uranio.types.ApiRequest) => {
 						console.log(api_request);
 						const urn_bll_errors = uranio.core.bll.basic.create('error');
-						return await urn_bll_errors.find({});
+						return await urn_bll_errors.find_by_id('611bedc525e7b32c3f6415d8');
 					}
 				}
 			}
@@ -39,5 +39,30 @@ export const atom_book:uranio.types.Book = {
 			console.log(some);
 			return new MYBLL();
 		}
+	},
+	kart:{
+		properties:{
+			email: {
+				type: uranio.types.BookPropertyType.EMAIL,
+				label: 'EMAIL'
+			}
+		},
+		api: {
+			url: "/karts",
+			routes: {
+				mycustom: {
+					url: '/c/:id/:date',
+					query: ['sick'],
+					action: uranio.types.AuthAction.READ,
+					method: uranio.types.RouteMethod.GET,
+					call: async (_api_request:uranio.types.ApiRequest) => {
+						// urn_log.fn_debug(`Router Call GET [find] / [${atom_name}]`);
+						const urn_bll = uranio.core.bll.basic.create('request');
+						const bll_res = await urn_bll.find_by_id('611bedc525e7b32c3f6415d8');
+						return bll_res;
+					}
+				}
+			}
+		},
 	}
 };
