fa5a84016112c0808e1817cd7307cfb5508ea07d
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-07-30 08:44:22 +0200

    working on panel start script.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
-import uranio from 'uranio';
+// import uranio from 'uranio';
 
-const pro_bll = uranio.core.bll.basic.create('product');
+// const pro_bll = uranio.core.bll.basic.create('product');
 
-console.log(pro_bll);
+// console.log(pro_bll);
