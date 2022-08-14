2c09480d38154c95b57edd446c194b57aa564231
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-08-14 20:48:19 +0200

    init. updated uranio.toml.

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,5 +3,9 @@
 // const pro_bll = uranio.core.bll.basic.create('product');
 
 // console.log(pro_bll);
+import uranio from 'uranio';
 
 console.log('THIS IS A CUSTOM LOG FROM THE SERVER.');
+
+console.log(uranio.conf.get_all());
+
